badfe1e820e2e6bcdba7d263eb313fb72e6f68cb
Taylor Shuff <tkylesh@gmail.com>
2016-09-02 14:35:38 -0500

    got all object array properties to display within container dive on page, working on css now

---
+++ wysiwyg.js
@@ -0,0 +1,64 @@
+// {
+//   title: "Samurai",
+//   name: "Tomoe Gozen",
+//   bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+//   image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
+//   lifespan: {
+//     birth: 1747,
+//     death: 1797
+//   }
+// }
+
+var ArrObjects = [
+{title: "Samurai",
+  name: "Tomoe Gozen",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  lifespan: {
+    birth: 1747,
+    death: 1797
+	}
+  },
+  {title: "Samurai",
+  name: "Tomoe Gozen",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  lifespan: {
+    birth: 1747,
+    death: 1797
+	}
+  },
+  {title: "Samurai",
+  name: "Tomoe Gozen",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  lifespan: {
+    birth: 1747,
+    death: 1797
+    }
+  }];
+  //testing object with console.log
+  console.log(ArrObjects);
+//decalare output element
+	var outputEl = document.getElementById('container');
+// add a new div section for each object in the array
+for (var i =0; i< ArrObjects.length; i++){
+	//Add each person element to container and give each a common class and unique Id
+	outputEl.innerHTML += `<div class="person_container" id="person--${i}"><person><header>
+	name: ${ArrObjects[i]["name"]}<br/>
+	title: ${ArrObjects[i]["title"]}<br/></header>
+	<section>
+	bio: ${ArrObjects[i]["bio"]}<br/>
+	image: ${ArrObjects[i]["image"]}<br/>
+	</section>
+	<footer>
+	birth: ${ArrObjects[i]["lifespan"]["birth"]}<br/>
+	death: ${ArrObjects[i]["lifespan"]["death"]}<br/>
+	</footer>
+	</person>
+	</div>`;
+}
+
+//Now containerEl will have elements in it 
+//creates an array that contains all elements with this class
+var containerEl = document.getElementsByClassName('person_container');
