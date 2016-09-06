21a77e1e24e0b08879de0f6e8028bebe2ec6e702
Taylor Shuff <tkylesh@gmail.com>
2016-09-06 09:04:44 -0500

    completed wysiwyg project

--- wysiwyg.js
+++ wysiwyg.js
@@ -48,7 +48,7 @@ for (var i =0; i< ArrObjects.length; i++){
 	name: ${ArrObjects[i]["name"]}<br/>
 	title: ${ArrObjects[i]["title"]}<br/></header>
 	<section>
-	bio: ${ArrObjects[i]["bio"]}<br/>
+	<div id="bio">bio: ${ArrObjects[i]["bio"]}</div><br/>
 	image: ${ArrObjects[i]["image"]}<br/>
 	</section>
 	<footer>
@@ -65,12 +65,34 @@ for (var i =0; i< ArrObjects.length; i++){
 //creates an array that contains all elements with this class
 var containerEl = document.getElementsByClassName('person_container');
 
+//keyup event listener
+document.getElementyById("edit").addEventListener("keyup",function(event){
+  if(event.keyCode === 13){
+    edit.value="";
+  } else {
+    document.getElementyById(targetSection).innerHTML = `bio: ${edit.value}`;
+  }
+})
+
 
 //Now you can create event listeners to the dom elements since they are already added to the page
 for(var i = 0; i< ArrObjects.length; i++){
 	containerEl[i].addEventListener("click", function(event){
+    //get innerHTML of section element
+    var sec = document.getElementById(event.target.id).getElementsByTagName("section")[0];
+    //set border of the section you clicked
+    document.getElementById(sec.id).style.border = "2px dotted yellow";
+    //set focus to input field
+    edit.focus();
+    //define target section
+    targetSection = sec.id;
+
+    edit.value = sec.innerHTML;
+
+
+
 		// 8. When you click on one of the person elements, a dotted border should appear around it.
 		// 9. When you click on one of the person elements, the text input should immediately gain focus so that you can start typing
-
+		
 	});
 }
